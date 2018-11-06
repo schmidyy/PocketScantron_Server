80b00608d2543e63fc16b99ec314031645b452ca
Seena Rowhani <seenarowhani95@gmail.com>
2018-11-06 13:29:33 -0500

    something like this

--- functions/src/api/produce-document-score.ts
+++ functions/src/api/produce-document-score.ts
@@ -2,6 +2,10 @@ import * as functions from 'firebase-functions';
 
 import fetch from 'node-fetch'
 
+import jimp = require('jimp')
+import tinycolor = require("tinycolor2");
+
+
 const NUM_COLUMNS = 7;
 const NUM_ROWS = 15;
 
@@ -9,6 +13,8 @@ const MAX_NUM = 100;
 
 const TOP_PADDING = 30;
 const CLUSTER_WIDTH = 30;
+
+const letters = ['A', 'B', 'C', 'D', 'E']
 
 const {
   computer_vision: subscriptionKey,
@@ -25,6 +31,10 @@ export function y (bb): number {
   return Number(y)
 }
 
+export function toHex(d) {
+  return  ("0"+(Number(d).toString(16))).slice(-2).toUpperCase()
+}
+
 const topGroup = Array(NUM_COLUMNS).fill(null)
   .map((_, i) => (15 * i) + 1)
 
@@ -36,12 +46,12 @@ const gridSystem = leftGroup.map(
     .map((_, j) => (15 * j) + i)
 ).reduce((acc, curr) => [...acc, ...curr], [])
 
-console.log(gridSystem)
+// console.log(gridSystem)
 
 export const documentScore = functions.https.onRequest(
   async ({ body }, response) => {
     const { url } = JSON.parse(body);
-    console.log(url)
+    // console.log(url)
 
     const cvResponse = await fetch(AZURE_CV_ENDPOINT, {
       method: 'POST',
@@ -80,40 +90,7 @@ export const documentScore = functions.https.onRequest(
       })
     , {})
 
-    console.log(numberTermsMap)
-
-    const filteredTopGroup = topGroup.filter(g => Boolean(numberTermsMap[g]))
-    const filteredLeftGroup = leftGroup.filter(g => Boolean(numberTermsMap[g]))
-
-    const bestTop = filteredTopGroup.reduce(
-      (acc, c) => {
-        const curr = y(numberTermsMap[c])
-        console.log('y', curr)
-        return acc + curr
-      }
-    , 0) / filteredTopGroup.length
-
-    const bestLeft = filteredLeftGroup.reduce(
-      (acc, c) => {
-        const curr = x(numberTermsMap[c])
-        console.log('x', curr)
-        return acc + curr
-      }
-    , 0) / filteredLeftGroup.length
-
-    let columnWidth = 0
-
-    for (let i = 0 ; i < filteredTopGroup.length - 1; i++) {
-      const curr = filteredTopGroup[i]
-      const next = filteredTopGroup[i + 1]
-
-      const deltaX = Math.abs(x(numberTermsMap[next]) - x(numberTermsMap[curr]))
-      columnWidth += deltaX
-    }
-
-    columnWidth /= filteredTopGroup.length;
-
-    const clusteredRowHeights = gridSystem.reduce(
+    const clusteredRowY = gridSystem.reduce(
       (acc, c) => {
         const bb = numberTermsMap[c]
 
@@ -136,18 +113,98 @@ export const documentScore = functions.https.onRequest(
       }
     , {})
 
-    const averageRowHeights = Object.keys(clusteredRowHeights)
-      .map(k => clusteredRowHeights[k])
+    const clusteredColX = gridSystem.reduce(
+      (acc, c) => {
+        const bb = numberTermsMap[c]
+
+        if (!bb) {
+          return acc
+        }
+
+        const _x = x(bb)
+
+        const clusterKey = Object.keys(acc).find(
+          cluster => Math.abs(Number(_x) - Number(cluster)) <= CLUSTER_WIDTH
+        );
+
+        if (clusterKey) {
+          acc[clusterKey].push(bb)
+        } else {
+          acc[_x] = [ bb ]
+        }
+        return acc
+      }
+    , {})
+
+    const averageRowY = Object.keys(clusteredRowY)
+      .map(k => clusteredRowY[k])
       .map(
-        cluster => cluster.reduce((a, c) => a + y(c), 0) / cluster.length
-      ).sort()
+        cluster => Number(cluster.reduce((a, c) => a + y(c), 0) / cluster.length)
+      ).sort((a, b) => a - b)
 
-    response.send(averageRowHeights)
+    const averageColX = Object.keys(clusteredColX)
+      .map(k => clusteredColX[k])
+      .map(
+        cluster => Number(cluster.reduce((a, c) => a + x(c), 0) / cluster.length)
+      ).sort((a, b) => a - b)
 
+    const image = await jimp.read(url)
+    // console.log(url)
+    let averageWidth = 0
+    let averageHeight = 0
 
-    averageRowHeights.forEach(i => {
+    for (let i = 0; i < averageColX.length - 1; i++) {
+      averageWidth += Math.abs(averageColX[i + 1] - averageColX[i])
+    }
 
+    averageWidth /= averageColX.length
+
+    for (let i = 0; i < averageRowY.length - 1; i++) {
+      averageHeight += Math.abs(averageRowY[i + 1] - averageRowY[i])
+    }
+
+    averageHeight /= averageRowY.length
+
+    image.blur(15)
+    console.log('blurred')
+    const chosenAnswers = []
+
+    console.log('avg-width', averageWidth)
+    console.log('avg-height', averageHeight)
+
+    averageRowY.forEach(y => {
+      averageColX.forEach(x => {
+        // image.pixelate(averageHeight / 2, x, y, averageWidth, averageHeight)
+        
+        let darkest = {
+          index: -1,
+          luminance: Infinity,
+        };
+
+        for (let i = 5; i > 0; i--) {
+          const pixelColor = image.getPixelColor(
+            x + (averageWidth / i) - 30,
+            y + (averageHeight / 2)
+          )
+          // console.log(pixelColor)
+
+          const { r, g, b, a } = jimp.intToRGBA(pixelColor)
+
+          const pixelLuminance = tinycolor(`rgba(${r}, ${g}, ${b}, ${a})`).getLuminance()
+          console.log(pixelLuminance)
+          if (darkest.luminance > pixelLuminance) {
+            darkest = {
+              index: 5 - i,
+              luminance: pixelLuminance
+            }
+          }
+        }
+        console.log(darkest)
+        chosenAnswers.push(letters[darkest.index])
+      })
     })
+
+    response.send(chosenAnswers)
 
     // const bestTop = {}
 
